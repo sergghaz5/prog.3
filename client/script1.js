a466477a1505585ea7f527a4accc6e11943fb928
sergghaz5 <sergey.ghazaryan5.y@tumo.org>
2023-05-22 17:27:01 +0400

    prog.3

--- client/script1.js
+++ client/script1.js
@@ -1,5 +1,51 @@
-function setup(){
-    frameRate(5);
-    createCanvas(400, 400)
+var socket=io()
+
+function setup() {
+
+    frameRate(60);
+    createCanvas(400,400);
     background('#acacac');
+}
+socket.on("matrix",my_draw);
+function m(a){
+    console.log(a);
+}
+
+function my_draw(matrix) {
+
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("black");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("lime");
+            }
+            // else if (matrix[y][x] == 6) {
+            //     fill("blue");
+            // }
+            rect(x * 50, y * 50, 50, 50);
+
+
+            // fill("blue")
+            // text(x + " " + y, x * side + side / 2, y * side + side / 2)
+
+        }
+    }
+
 }
--- server/server.js
+++ server/server.js
@@ -1,40 +1,95 @@
 var express = require("express");
-
 var app = express();
-
 app.use(express.static("../client"));
-
-app.get("/", function(req, res){
-
-res.redirect("index.html");
-
+app.get("/", function (req, res) {
+  res.redirect("index.html");
 });
 
-app.listen(3000, function(){
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-console.log("Game is running on port 3000");
-
+server.listen(3000, function () {
+  console.log("Game is running on port 3000");
 });
 var Grass = require("./grass")
 var GrassEater = require("./grassEater")
-var Predator = require("./predator")
-var PredatorBlue = require("./predatorBlue")
-var PredatorEater = require("./predatorEater")
-
-
+var predatorBlue = require("./predatorBlue")
+var Predator = require("./Predator")
+var predatorEater = require("./predatorEater")
+// var { generateMatrix } = require("./functions")
 matrix = []
-function getRandInt(min,max){
-    var z = Math.floor(Math.random()*(max-min+1)) + min;
+grassArr = []
+grassEaterArr = []
+predatorArr = []
+predatorBlueArr = []
+predatorEaterArr = []
 
-return z;
-}
-function generateMatrix(){
-    for(let d = 0; d < 50; d++){
-        matrix[d] = []
-        for (let g = 0; g < 70; g++){
-          matrix[d][g] = getRandInt(0,5)
-        }
-      }
-    }
+
+io.on('connection', function (socket) {
+  socket.emit("matrix", matrix)
+});
+
 generateMatrix()
-console.log(matrix);
+createObject()
+// generateMatrix()
+console.log(matrix)
+io.sockets.emit("matrix", matrix)
+
+var gr = new Grass(1, 2, 1);
+var emptyCells = gr.chooseCell(0);
+console.log(emptyCells);
+
+function createObject() {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] == 1) {
+        let gr = new Grass(x, y, 1)
+        grassArr.push(gr)
+      }
+      else if (matrix[y][x] == 2) {
+        let grassE = new GrassEater(x, y, 2)
+        grassEatArr.push(grassE)
+      }
+      else if (matrix[y][x] == 3) {
+        let predatorObj = new Predator(x, y, 3)
+        predatorArr.push(predatorObj)
+      }
+      else if (matrix[y][x] == 4) {
+        let predatorEaterObj = new Predator(x, y, 4)
+        predatorEaterArr.push(predatorEaterObj)
+      }
+      else if (matrix[y][x] == 4) {
+        let predatorBlueObj = new predatorBlue(x, y, 5)
+        predatorBlueArr.push(predatorBlueObj)
+      }
+
+    }
+  }
+}
+
+
+function game() {
+  for (let i in grassArr) {
+    grassArr[i].mul()
+  }
+  for (let i in grassEatArr) {
+    grassEatArr[i].eat()
+  }
+
+  for (let i in predatorArr) {
+    predatorArr[i].eat()
+  }
+
+  for (let i in predatorEaterArr) {
+    predatorEaterArr[i].eat()
+  }
+  for (let i in predatorBlueArr) {
+    predatorBlueArr[i].eat()
+  }
+  io.sockets.emit("matrix", matrix)
+}
+
+
+
+
+setInterval(game, 2000)
